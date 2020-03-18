87d202cfd6dd867896c1f1c1d3d222e02e69da26
Klaviam <klaviam@gmail.com>
2020-03-17 19:48:46 -0700

    Sandbox level

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] float chaseRange = 5.0f;
+    private float distanceToTarget = Mathf.Infinity;
+    private Transform target = null;
+    private NavMeshAgent navMeshAgent;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        target = FindObjectOfType<Player>().transform;
+    }
+
+    void Update()
+    {
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+
+        if (distanceToTarget < chaseRange)
+        {
+            navMeshAgent.SetDestination(target.position);
+        }
+    }
+}
